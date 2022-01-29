9a5e2dfe04b5c87830929f84317dd0f9265063d4
Gesha <geshamat1egogey@mail.ru>
2022-01-29 11:56:40 +0300

    сука

--- caffeine/catalog/views.py
+++ caffeine/catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import CustomUser, Article
 from django.views.generic.edit import FormView
-from .forms import CustomUserRegistrationForm, Questionary1, Questionary2, Questionary3
+from .forms import CustomUserRegistrationForm, Questionary1, Questionary2, Questionary3, Questionary4
 
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
@@ -50,21 +50,31 @@ def statistic_view(request):
 
 
 def articles_view(request):
-    return render(request, 'articles.html')
+    num = Article.objects.all().count()
+    articles_list = [i.create_dict() for i in Article.objects.all()]
+    return render(request, 'articles.html', {'num': num, 'articles_list': articles_list})
 
 
-def questionary_view(request, data={}):
-    form = Questionary1()
+def questionary_view(request, data={'n': 1}):
     req = None
+    form = Questionary1()
     if request.method == 'POST':
         req = request.POST
-        if form.numb == 1:
+        if data['n'] == 1:
             data.update(req)
+            data['n'] = 2
             form = Questionary2()
-        elif form.numb == 2:
+        elif data['n'] == 2:
             data.update(req)
+            data['n'] = 3
             form = Questionary3()
-        elif form.numb == 3:
+        elif data['n'] == 3:
             data.update(req)
+            data['n'] = 4
+            form = Questionary4()
+        elif data['n'] == 3:
+            data.update(req)
+            data['n'] = 4
+            form = Questionary4()
 
     return render(request, 'questionary_base.html', context={'form': form, 'req': req, 'data': data})
